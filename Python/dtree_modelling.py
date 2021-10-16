e0263394e76eddfcd7997382669f639311ae90eb
DESKTOP-AM4-JF <joseph_foley@hotmail.com>
2021-10-16 20:49:17 +0100

    train tree and gen tree on preprocessed data

--- Python/dtree_modelling.py
+++ Python/dtree_modelling.py
@@ -6,12 +6,19 @@ Backend of the project. This will make the dtree model and generate the plot
 # IMPORTS
 # =============================================================================
 import pandas as pd
-from sklearn.tree import DecisionTreeClassifier, plot_tree
+import matplotlib.pyplot as plt
+from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor,\
+                         plot_tree
+                         
 
 # =============================================================================
 # CONSTANTS
 # =============================================================================
-data_path = '../Data/Bike share data (Featurised).csv'
+DATA_PATH = '../Data/Bike share data (Featurised).csv'
+TREE_DEPTH = 3
+PROB = 'regression'
+
+class_names = None
 
 # =============================================================================
 # FUNCTIONS
@@ -21,7 +28,7 @@ def loadData(data_path):
     TEMP: pandas load csv
     FUTURE: streamlit pass thru
     '''
-    df = pd.read_csv(data_path)
+    df = pd.read_csv(DATA_PATH)
     
     return df
 
@@ -49,13 +56,66 @@ def pickResponse(cols):
     
     return response
     
+def catOrCont(df):
+    '''
+    Determine which columns are categorical and which are not
+    '''
+    return dtype_dict
 
+def getClassNames(df, response):
+    '''
+    Get the class names of a classification problem
+    '''
+    if PROB == 'regression':
+        class_names = None
+    else:
+        pass
+    
+    return class_names
+
+def processData(df):
+    '''
+    Combines preprocessing functions to make data ready for modelling
+    '''
+    return df
+
+def trainTree(df, PROB, response):
+    '''
+    train a dtree model
+    '''
+    #regression or classification
+    if PROB == 'regression':
+        dtree = DecisionTreeRegressor(max_depth=TREE_DEPTH)
+        
+    elif PROB == 'classification':
+        dtree = DecisionTreeClassifier(max_depth=TREE_DEPTH)
+        
+    else:
+        print('\n\n\n!!! PROB must be regression or classification !!!\n\n\n')
+        
+    #train
+    dtree.fit(df.drop(response, axis=1), df[response])
+    
+    return dtree
+
+def genTree(dtree):
+    '''
+    generates (& displays) a drawn dtree
+    '''
+    plt.figure(figsize=(20, 12)) 
+    plot_tree(dtree, feature_names=df.drop(response, axis=1).columns,\
+              class_names=class_names, filled=True, rounded=True, precision=2,\
+              proportion=True)
+        
 # =============================================================================
 # EXECUTE
 # =============================================================================
 if __name__ =='__main__':
-    df = loadData(data_path)
+    df = loadData(DATA_PATH)
     cols = getColumns(df)
     response = pickResponse(cols)
+    class_names = getClassNames(df, response)
+    dtree = trainTree(df, PROB, response)
+    genTree(dtree)
     
     
